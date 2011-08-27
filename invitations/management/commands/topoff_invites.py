1291d1ea85ae86cc35eadc3bb83045c3a9f9b0fc
Patrick Altman <paltman@gmail.com>
2011-08-27 08:54:05 -0500

    added topoff_invites command

---
+++ invitations/management/commands/topoff_invites.py
@@ -0,0 +1,27 @@
+import sys
+
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth.models import User
+
+from invitations.models import InvitationStat
+
+
+class Command(BaseCommand):
+    help = "Makes sure all users have a certain number of invites."
+    
+    def handle(self, *args, **kwargs):
+        if len(args) == 0:
+            sys.exit("You must supply the number of invites as an argument.")
+        
+        try:
+            num_of_invites = int(args[0])
+        except ValueError:
+            sys.exit("The argument for number of invites must be an integer.")
+        
+        for user in User.objects.all():
+            stat, _ = InvitationStat.objects.get_or_create(user=user)
+            remaining = stat.invites_remaining()
+            if remaining < num_of_invites:
+                stat.invites_allocated += (num_of_invites - remaining)
+                stat.save()
