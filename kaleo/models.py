7b9ae4e2a63b61f666278c7a4f9245969ca782e5
Brian Rosner <brosner@gmail.com>
2012-03-13 16:10:59 -0700

    added missing import

--- kaleo/models.py
+++ kaleo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.conf import settings
+from django.dispatch import receiver
 from django.utils import timezone
 
 from django.contrib.auth.models import User
