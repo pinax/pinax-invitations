1fcb2818b5f6c1623fec841ddd994a800aefc5d4
Brian Rosner <brosner@gmail.com>
2012-11-29 00:18:34 -0700

    Improved HTTP response when user not authenticated
    Returning a HTTP redirect from login_required is designed for the web browser
    and a human as the end-user; invite is used by JavaScript. Return an error
    more suitable for the end-user.

--- kaleo/views.py
+++ kaleo/views.py
@@ -2,16 +2,16 @@ from django import http
 from django.utils import simplejson as json
 from django.views.decorators.http import require_http_methods
 
-from django.contrib.auth.decorators import login_required
-
 from account.models import EmailAddress
 from kaleo.forms import InviteForm
 from kaleo.models import JoinInvitation
 
 
-@login_required
 @require_http_methods(["POST"])
 def invite(request):
+    if not request.user.is_authenticated():
+        data = {"status": "ERROR", "message": "not authenticated"}
+        return http.HttpResponseBadRequest(json.dumps(data), content_type="application/json")
     form = InviteForm(request.POST)
     if form.is_valid():
         email = form.cleaned_data["email_address"]
