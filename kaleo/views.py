a2563996ba3642e2898097256a3b1e689845e86f
Patrick Altman <paltman@gmail.com>
2013-01-13 18:21:21 -0600

    Rewrite view to support bootstrap-ajax
    A secondary benefit to this is the separation of
    concerns between the form and the view. The
    validation logic was relocated to live in the form.
    
    As a result the form now requires a user object
    upon instantiation.

--- kaleo/views.py
+++ kaleo/views.py
@@ -1,9 +1,10 @@
 import json
 
 from django import http
+from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
 
-from account.models import EmailAddress
 from django.contrib.auth.decorators import login_required
 
 from kaleo.forms import InviteForm
@@ -13,20 +14,26 @@ from kaleo.models import JoinInvitation
 @login_required
 @require_POST
 def invite(request):
-    form = InviteForm(request.POST)
+    form = InviteForm(request.POST, user=request.user)
     if form.is_valid():
         email = form.cleaned_data["email_address"]
-        if EmailAddress.objects.filter(email=email, verified=True).exists():
-            data = {"status": "ERROR", "errors": '<ul class="errorlist"><li>%s is already on this site</li></ul>' % email}
-        elif JoinInvitation.objects.filter(from_user=request.user, signup_code__email=email).exists():
-            data = {"status": "ERROR", "errors": '<ul class="errorlist"><li>You have already invited %s</li></ul>' % email}
-        else:
-            JoinInvitation.invite(request.user, email)
-            data = {
-                "status": "OK",
-                "email": email,
-                "invitations_remaining": request.user.invitationstat.invites_remaining()
-            }
-    else:
-        data = {"status": "ERROR", "errors": str(form.errors["email_address"])}
+        JoinInvitation.invite(request.user, email)
+        form = InviteForm(user=request.user)
+    data = {
+        "html": render_to_string("kaleo/_invite_form.html", {
+                "form": form,
+                "user": request.user
+            }, context_instance=RequestContext(request)
+        ),
+        "fragments": {
+            ".kaleo-invites-remaining": render_to_string("kaleo/_invites_remaining.html", {
+                    "invites_remaining": request.user.invitationstat.invites_remaining()
+                }, context_instance=RequestContext(request)
+            ),
+            ".kaleo-invites-sent": render_to_string("kaleo/_invited.html", {
+                    "invited_list": request.user.invites_sent.all()
+                }, context_instance=RequestContext(request)
+            )
+        }
+    }
     return http.HttpResponse(json.dumps(data), content_type="application/json")
