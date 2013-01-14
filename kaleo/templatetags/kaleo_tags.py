9cc7b959d18761ef354260fd2e2dab8b3cf9611d
Patrick Altman <paltman@gmail.com>
2013-01-13 18:15:58 -0600

    Replace remaining_invites template tag
    Replaced in favor of a simple inclusion tag called
    invites_remaining that will ship with a sane default
    template.

--- kaleo/templatetags/kaleo_tags.py
+++ kaleo/templatetags/kaleo_tags.py
@@ -7,41 +7,18 @@ from kaleo.models import InvitationStat
 register = template.Library()
 
 
-class RemainingInvitesNode(template.Node):
-    
-    @classmethod
-    def handle_token(cls, parser, token):
-        bits = token.split_contents()
-        if len(bits) != 2:
-            raise template.TemplateSyntaxError
-        
-        return cls(user=parser.compile_filter(bits[1]))
-    
-    def __init__(self, user):
-        self.user = user
-    
-    def render(self, context):
-        user = self.user.resolve(context)
-        try:
-            return user.invitationstat.invites_remaining()
-        except InvitationStat.DoesNotExist:
-            return "0"
-
-
-@register.tag
-def remaining_invites(parser, token):
-    """
-    Usage::
-        {% remaining_invites user %}
-    
-    Returns an integer that is the # of remaining invites the user has.
-    """
-    return RemainingInvitesNode.handle_token(parser, token)
+@register.inclusion_tag("kaleo/_invites_remaining.html")
+def invites_remaining(user):
+    try:
+        remaining = user.invitationstat.invites_remaining()
+    except InvitationStat.DoesNotExist:
+        remaining = 0
+    return {"invites_remaining": remaining}
 
 
 @register.inclusion_tag("kaleo/_invite_form.html")
 def invite_form(user):
-    return {"form": InviteForm(), "user": user}
+    return {"form": InviteForm(user=user), "user": user}
 
 
 @register.inclusion_tag("kaleo/_invited.html")
