60a5dbb9634b2cdb1d4c5dc80589801cb670d4fc
Nathaniel Tucker <me@ntucker.me>
2013-02-14 16:37:56 -0800

    Support Django 1.5 custom user models

---
+++ kaleo/compat.py
@@ -0,0 +1,9 @@
+import django
+__all__ = ['get_user_model']
+
+# Django 1.5+ compatibility
+if django.VERSION >= (1, 5):
+    from django.contrib.auth import get_user_model
+else:
+    from django.contrib.auth.models import User
+    get_user_model = lambda:User
--- kaleo/management/commands/infinite_invites.py
+++ kaleo/management/commands/infinite_invites.py
@@ -2,16 +2,15 @@ import sys
 
 from django.core.management.base import BaseCommand
 
-from django.contrib.auth.models import User
-
 from kaleo.models import InvitationStat
+from .compat import get_user_model
 
 
 class Command(BaseCommand):
     help = "Sets invites_allocated to -1 to represent infinite invites."
     
     def handle(self, *args, **kwargs):
-        for user in User.objects.all():
+        for user in get_user_model().objects.all():
             stat, _ = InvitationStat.objects.get_or_create(user=user)
             stat.invites_allocated = -1
             stat.save()
