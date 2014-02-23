bcd9e64d260216ec70161ff0fc42d31f0cbfc2d7
Patrick Altman <paltman@gmail.com>
2014-02-23 11:15:37 -0600

    DUA needs to be be in INSTALLED_APPS

--- runtests.py
+++ runtests.py
@@ -12,6 +12,7 @@ DEFAULT_SETTINGS = dict(
         "django.contrib.auth",
         "django.contrib.contenttypes",
         "django.contrib.sites",
+        "account",
         "kaleo",
         "kaleo.tests"
     ],
