52a727eb42bf106b666db5aaf099ad25c68fb85b
Patrick Altman <paltman@gmail.com>
2014-02-23 11:19:07 -0600

    Make sure receivers are activated for tests

--- kaleo/tests/tests.py
+++ kaleo/tests/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from django.contrib.auth.models import User
 
+import kaleo.receivers  # noqa
+
 from account.models import SignupCode
 from kaleo.models import JoinInvitation
 
