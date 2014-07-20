3c4b716a8a1636a867130719729f22b4a1f16757
James Tauber <jtauber@jtauber.com>
2014-07-20 14:50:18 -0400

    keep flake8 happy

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,12 @@ exclude_patterns = ["_build"]
 pygments_style = "sphinx"
 html_theme = "default"
 htmlhelp_basename = "%sdoc" % project
-latex_documents = [
-  ("index", "%s.tex" % project, u"%s Documentation" % project,
-   copyright_holder, "manual"),
-]
-man_pages = [
-    ("index", project, u"%s Documentation" % project,
-     [copyright_holder], 1)
-]
+latex_documents = [(
+    "index", "%s.tex" % project, u"%s Documentation" % project, copyright_holder, "manual"
+)]
+man_pages = [(
+    "index", project, u"%s Documentation" % project, [copyright_holder], 1
+)]
 
 sys.path.insert(0, os.pardir)
 m = __import__("kaleo")
