04a4a08e02ba82a9c680b6d9a5686f1ed6d8b6ed
Jacob Wegner <jacobwegner@gmail.com>
2015-09-10 13:10:10 -0500

    Pin to initial migration for django-user-accounts

--- pinax/invitations/migrations/0001_initial.py
+++ pinax/invitations/migrations/0001_initial.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('account', '0002_auto_20150909_0234'),
+        ('account', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
