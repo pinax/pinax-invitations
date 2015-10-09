ca38135fd64b35d74fa903acd63656ca199ce997
Patrick Altman <paltman@gmail.com>
2015-10-09 07:54:51 -0400

    3.0.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="a user to user join invitations app",
     name="pinax-invitations",
     long_description=read("README.rst"),
-    version="2.2.0.dev1",
+    version="3.0.0",
     url="http://github.com/pinax/pinax-invitations/",
     license="MIT",
     packages=find_packages(),
