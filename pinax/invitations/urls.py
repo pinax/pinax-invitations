27e2b8a51a3ebebb11a34c9f9de4138467f4ef14
Patrick Altman <paltman@gmail.com>
2015-12-10 10:35:38 -0600

    Update for Django 1.9

--- pinax/invitations/urls.py
+++ pinax/invitations/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 
 from .views import (
     addto_all,
@@ -10,12 +10,11 @@ from .views import (
 )
 
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(r"^invite/$", invite, name="pinax-invitations-invite"),
     url(r"^invite-stat/(?P<pk>\d+)/$", invite_stat, name="pinax-invitations-invite_stat"),
     url(r"^topoff/$", topoff_all, name="pinax-invitations-topoff_all"),
     url(r"^topoff/(?P<pk>\d+)/$", topoff_user, name="pinax-invitations-topoff_user"),
     url(r"^addto/$", addto_all, name="pinax-invitations-addto_all"),
     url(r"^addto/(?P<pk>\d+)/$", addto_user, name="pinax-invitations-addto_user"),
-)
+]
