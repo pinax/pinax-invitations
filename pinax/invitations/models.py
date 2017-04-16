882e18b5fdf752c6de969c60771c830ae62ccc14
Patrick Altman <paltman@gmail.com>
2017-04-16 18:15:15 -0500

    Fix up imports

--- pinax/invitations/models.py
+++ pinax/invitations/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
+
+from django.contrib.auth import get_user_model
 
 from account.models import SignupCode
 
