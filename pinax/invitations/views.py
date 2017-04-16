64dea529a008460ebd92f7574e91ee43fd5d5666
Patrick Altman <paltman@gmail.com>
2017-04-16 18:12:05 -0500

    Fix up imports

--- pinax/invitations/views.py
+++ pinax/invitations/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.utils.decorators import method_decorator
-from django.views.generic import FormMixin, View
+from django.views.generic import View
+from django.views.generic.edit import FormMixin
+
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import permission_required
 
 from account.mixins import LoginRequiredMixin
