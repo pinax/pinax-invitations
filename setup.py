8b5a2bbac0047bb31400251e8c5df24c2ca87e92
KatherineMichel <kthrnmichel@gmail.com>
2017-12-21 14:38:03 -0600

    Fixing license link

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ Pinax Invitations
 .. image:: http://slack.pinaxproject.com/badge.svg
     :target: http://slack.pinaxproject.com/
 .. image:: https://img.shields.io/badge/license-MIT-blue.svg
-    :target: https://pypi.python.org/pypi/pinax-app/
+    :target: https://pypi.python.org/pypi/pinax-invitations/
 
 \ 
 
