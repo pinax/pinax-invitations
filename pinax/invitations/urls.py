a80562babcc1abeadd1fa6d36dbc003e826a3d9d
Christopher Broderick <chris@chris-mbp>
2023-04-14 17:46:06 +0100

    Changes to support Django 4

--- pinax/invitations/urls.py
+++ pinax/invitations/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import (
     AddToAllView,
@@ -12,10 +12,10 @@ from .views import (
 app_name = "pinax_invitations"
 
 urlpatterns = [
-    url(r"^invite/$", InviteView.as_view(), name="invite"),
-    url(r"^invite-stat/(?P<pk>\d+)/$", InviteStatView.as_view(), name="invite_stat"),
-    url(r"^topoff/$", TopOffAllView.as_view(), name="topoff_all"),
-    url(r"^topoff/(?P<pk>\d+)/$", TopOffUserView.as_view(), name="topoff_user"),
-    url(r"^addto/$", AddToAllView.as_view(), name="addto_all"),
-    url(r"^addto/(?P<pk>\d+)/$", AddToUserView.as_view(), name="addto_user"),
+    re_path(r"^invite/$", InviteView.as_view(), name="invite"),
+    re_path(r"^invite-stat/(?P<pk>\d+)/$", InviteStatView.as_view(), name="invite_stat"),
+    re_path(r"^topoff/$", TopOffAllView.as_view(), name="topoff_all"),
+    re_path(r"^topoff/(?P<pk>\d+)/$", TopOffUserView.as_view(), name="topoff_user"),
+    re_path(r"^addto/$", AddToAllView.as_view(), name="addto_all"),
+    re_path(r"^addto/(?P<pk>\d+)/$", AddToUserView.as_view(), name="addto_user"),
 ]
